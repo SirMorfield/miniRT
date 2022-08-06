d0c4438db71f6136cd398de81fc09df2b621e85e
SirMorfield <jkoers@student.codam.nl>
2022-08-06 14:12:35 +0200

    feat: more info

--- benchmark.sh
+++ benchmark.sh
@@ -5,5 +5,6 @@ echo >> history.txt
 echo >> history.txt
 printf 'Commit hash: ' >> history.txt; git rev-parse HEAD >> history.txt
 printf 'Date       : ' >> history.txt; date >> history.txt
+printf 'uname -a   : ' >> history.txt; uname -a >> history.txt
 
 ./miniRT rt/dragon.rt | tee -a history.txt
