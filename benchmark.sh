0e6da4fc901ac9bc61fb18138cdc0d988129e8de
SirMorfield <jkoers@student.codam.nl>
2022-08-09 00:59:49 +0200

    fix: benchmark script not pipeing

--- benchmark.sh
+++ benchmark.sh
@@ -7,7 +7,5 @@ printf 'Commit hash: ' >> history.txt; git rev-parse HEAD >> history.txt
 printf 'Date       : ' >> history.txt; date >> history.txt
 printf 'uname -a   : ' >> history.txt; uname -a >> history.txt
 
-CMD="./miniRT rt/dragon.rt | tee -a history.txt"
-printf 'Command    : ' >> history.txt; echo $CMD >> history.txt
-
-sh -c $CMD
+printf 'Command    : ' >> history.txt; echo "./miniRT rt/dragon.rt" >> history.txt
+./miniRT rt/dragon.rt | tee -a history.txt
