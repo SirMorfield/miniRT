09e22bedad40b9f6ff84d44a0eb574c587a7de84
Joppe Koers <joppe@joppekoers.nl>
2022-12-09 00:18:40 +0100

    Improve tests

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-echo ""
-echo "c -O0"
-gcc -O0 normalize.c
-./a.out
-echo ""
 echo "cpp -O0"
-clang++ -O0 normalize.cpp
+clang++ -O0 -std=c++17 normalize.cpp
 ./a.out
 
 echo ""
-echo "c -O3"
-gcc -O3 normalize.c
-./a.out
-echo ""
 echo "cpp -O3"
-clang++ -O3 normalize.cpp
+clang++ -O3 -std=c++17 normalize.cpp
 ./a.out
--- benchmark/normalize.cpp
+++ benchmark/normalize.cpp
@@ -1,17 +1,16 @@
+#include <chrono>
 #include <cmath>
+#include <iostream>
 #include <vector>
 
-#define COUNT 200000000
+#define COUNT 400000000
 
-double q_rsqrt(double number)
-{
-	static_assert(std::numeric_limits<double>::is_iec559,
-				  "fast inverse square root requires IEEE-comliant 'double'");
-	static_assert(sizeof(double) == sizeof(std::uint64_t),
-				  "fast inverse square root requires 'float' to be 64-bit");
+double q_rsqrt(double number) {
+	static_assert(std::numeric_limits<double>::is_iec559, "fast inverse square root requires IEEE-comliant 'double'");
+	static_assert(sizeof(double) == sizeof(std::uint64_t), "fast inverse square root requires 'float' to be 64-bit");
 
-	double x2 = number * 0.5;
-	double y = number;
+	double	 x2 = number * 0.5;
+	double	 y = number;
 	uint64_t i;
 	std::memcpy(&i, &y, sizeof(number));
 	i = 0x5fe6eb50c7b537a9 - (i >> 1);
@@ -20,21 +19,18 @@ double q_rsqrt(double number)
 	return y * (1.5 - (x2 * y * y));
 }
 
-class Vec3
-{
-public:
+class Vec3 {
+  public:
 	Vec3() {}
 
-	void normalize()
-	{
+	void normalize() {
 		double invLen = 1 / std::sqrt(x * x + y * y + z * z);
 		x *= invLen;
 		y *= invLen;
 		z *= invLen;
 	}
 
-	void q_normalize()
-	{
+	void q_normalize() {
 		double invLen = q_rsqrt(x * x + y * y + z * z);
 		x *= invLen;
 		y *= invLen;
@@ -46,28 +42,27 @@ public:
 	double z;
 };
 
-int main()
-{
+int main() {
 	std::vector<Vec3> vecs;
 	vecs.resize(COUNT);
 
 	{
-		clock_t start = clock();
-		for (size_t i = 0; i < COUNT; i++)
-		{
+		auto start = std::chrono::high_resolution_clock::now();
+		for (size_t i = 0; i < COUNT; i++) {
 			vecs[i].normalize();
 		}
-		double elapsed = (double)(clock() - start) / CLOCKS_PER_SEC;
-		printf("normalize(): %.5f seconds\n", elapsed);
+		auto   elapsed = std::chrono::high_resolution_clock::now() - start;
+		double ms_elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count();
+		std::cout << "normalize(): " << ms_elapsed << " ms" << std::endl;
 	}
 	{
-		clock_t start = clock();
-		for (size_t i = 0; i < COUNT; i++)
-		{
+		auto start = std::chrono::high_resolution_clock::now();
+		for (size_t i = 0; i < COUNT; i++) {
 			vecs[i].q_normalize();
 		}
-		double elapsed = (double)(clock() - start) / CLOCKS_PER_SEC;
-		printf("q_normalize(): %.5f seconds\n", elapsed);
+		auto   elapsed = std::chrono::high_resolution_clock::now() - start;
+		double ms_elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count();
+		std::cout << "q_normalize(): " << ms_elapsed << " ms" << std::endl;
 	}
 
 	return 0;
